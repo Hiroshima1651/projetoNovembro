29732f0042319fdd269c17a91f779552fbad72ac
Hiroshima1651 <luishiroshidw@gmail.com>
2025-11-26 10:40:45 -0300

    correção

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("loja-link").addEventListener("click", () => {
+// Adicione aqui o redirecionamento da aba LOJA.
+console.log("Loja será redirecionada aqui.");
+});
+
+
+document.getElementById("confiapay-link").addEventListener("click", () => {
+// Adicione aqui o redirecionamento da aba CONFIAPAY.
+console.log("ConfiaPay será redirecionado aqui.");
+});
